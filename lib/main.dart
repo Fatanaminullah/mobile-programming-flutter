df6c3f14bd477d49caaaa03075242ee36c65edd0
fatan <fatan.aminullah.j@gmail.com>
2022-01-08 03:02:33 +0700

    finishing uas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,57 +7,83 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Tugas Besar 2',
-      home: MyCustomForm(),
+    const appTitle = 'UAS';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(appTitle),
+          ),
+          body: const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: MyForm(),
+          )),
     );
   }
 }
 
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({Key? key}) : super(key: key);
+class FormData {
+  final String nama;
+  final String nim;
 
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
+  FormData(this.nama, this.nim);
 }
 
-class _MyCustomFormState extends State<MyCustomForm> {
-  final myController = TextEditingController();
+class MyForm extends StatefulWidget {
+  const MyForm({Key? key}) : super(key: key);
 
   @override
-  void dispose() {
-    myController.dispose();
-    super.dispose();
+  MyFormState createState() {
+    return MyFormState();
   }
+}
+
+class MyFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+  final nama = TextEditingController();
+  final nim = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tugas Besar 2'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: myController,
-          decoration: InputDecoration(hintText: 'Nama Lengkap'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const ConfirmationScreen(),
-              // Pass the arguments as part of the RouteSettings. The
-              // DetailScreen reads the arguments from these settings.
-              settings: RouteSettings(
-                arguments: myController.text,
-              ),
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Image.network(
+                "https://upload.wikimedia.org/wikipedia/id/1/1d/Logo_umc.gif",
+                fit: BoxFit.fill),
+          ),
+          TextField(
+            controller: nama,
+            decoration: const InputDecoration(hintText: 'Nama Lengkap'),
+          ),
+          TextField(
+            controller: nim,
+            decoration: const InputDecoration(hintText: 'NIM'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(fixedSize: const Size(400, 40)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ConfirmationScreen(),
+                    settings:
+                        RouteSettings(arguments: FormData(nama.text, nim.text)),
+                  ),
+                );
+              },
+              child: const Text('Masuk'),
             ),
-          );
-        },
-        child: const Icon(Icons.text_fields),
+          ),
+        ],
       ),
     );
   }
@@ -70,7 +93,7 @@ class ConfirmationScreen extends StatelessWidget {
   const ConfirmationScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final data = ModalRoute.of(context)!.settings.arguments;
+    final data = ModalRoute.of(context)!.settings.arguments as FormData;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Konfirmasi"),
@@ -81,17 +104,30 @@ class ConfirmationScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              padding: const EdgeInsets.only(bottom: 8),
+              padding: const EdgeInsets.only(bottom: 4),
               child: const Text(
                 'Nama Lengkap',
                 style: TextStyle(
-                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
                 ),
               ),
             ),
             Text(
-              data.toString(),
-              style: TextStyle(fontSize: 20),
+              data.nama,
+              style: TextStyle(fontSize: 22),
+            ),
+            Container(
+              padding: const EdgeInsets.only(bottom: 4, top: 20),
+              child: const Text(
+                'NIM',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            Text(
+              data.nim,
+              style: TextStyle(fontSize: 22),
             ),
           ],
         ),
